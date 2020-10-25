7b4f18392802934ecfe78674aeea23ff0f3bf5c7
ashish8796 <ashishsaini.ak@gmail.com>
2020-10-25 21:39:14 +0530

    Remove unused files

--- src/js/stores/states/timer.state.js
+++
@@ -1,9 +0,0 @@
-const timerState = localStorage.hasOwnProperty("userTimer") ? JSON.parse(localStorage.getItem("userTimer")) : {
-  el: null,
-  statePoint: '',
-  isAmPmVisible: false,
-  amPm: "",
-  amPmElemDisplay: "none",
-}
-
-export default timerState;
--- src/js/stores/states/todos.state.js
+++
@@ -1,8 +0,0 @@
-const state = localStorage.hasOwnProperty("userTodos") ? JSON.parse(localStorage.getItem("userTodos")) : {
-  todos: [],
-  currentActiveTodo: null,
-  lastActiveTodo: null,
-  isSelectVisible: false
-}
-
-export default state;
