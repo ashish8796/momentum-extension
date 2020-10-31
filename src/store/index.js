3037fca733f4a6e3a07a5e2673db54956a534786
ashish8796 <ashishsaini.ak@gmail.com>
2020-10-31 13:01:00 +0530

    Store is fixed

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, createStore, compose } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import rootReducer from "./reducers/reducers";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
