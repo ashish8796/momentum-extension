f27d506074517bb7717130b562c767ae735c2040
ashish8796 <ashishsaini.ak@gmail.com>
2021-03-16 12:45:52 +0530

    extension working properly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { store } from "./store";
 import Background from "./components/Background";
 import { store } from "./store";
 
